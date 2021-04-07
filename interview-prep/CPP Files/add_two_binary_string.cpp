f7cd051f3effa2f27402604e54e17516792e2145
anshajsharma <anshajkumarsharma@gmail.com>
2021-04-07 21:48:15 +0530

    Update

---
+++ interview-prep/CPP Files/add_two_binary_string.cpp
@@ -0,0 +1,23 @@
+    string addBinary(string num1, string num2) {
+       std::reverse(num1.begin(), num1.end());
+        std::reverse(num2.begin(), num2.end());
+        int n=num1.size(),m=num2.size();
+        int carry=0;
+        int n1=0,n2=0;
+        string ans="";
+        int i=0,j=0;
+        while(i<n || j<m)
+        {
+            if(i<n) { n1 = num1[i]-'0' ; i++;}
+            if(j<m) { n2 = num2[j]-'0' ; j++;}
+            ans = char( (n1+n2+carry)%2  + '0' ) + ans;
+            carry = (n1+n2+carry)/2;
+            if(i<n || j<m) {
+                 
+            }else if(carry>0){
+                ans =  char( (carry)%2  + '0' ) + ans;
+            }
+            n1=0,n2=0;
+        }
+        return ans; 
+    }
--- interview-prep/ReadMe.md
+++ interview-prep/ReadMe.md
@@ -8,7 +8,7 @@
 | No. | Topics | 
 | --- | --------- |
 |1   | [Add two string(numerical sum)](https://github.com/anshajsharma/CP-Algorithms/blob/master/interview-prep/CPP%20Files/numerical_sum_of_strings.cpp) | 
-|2   | [Dynamic Programming](https://github.com/anshajsharma/cp-nitjsr/blob/main/topics/DP.md) |
+|2   | [Add two binary string](https://github.com/anshajsharma/cp-nitjsr/blob/main/topics/DP.md) |
 |3   | [Greedy](https://github.com/anshajsharma/cp-nitjsr/blob/main/topics/Greedy.md) |
 |4   | [Tree](#) |
 |5   | [Graph](https://github.com/anshajsharma/cp-nitjsr/blob/main/topics/Graph.md) |
